bdf7686f2f6e5fdcd37e46bbd9ce83d92a627778
Adrian Halaburda <adh761@gmail.com>
2020-10-02 09:50:05 -0400

    Some minor change at javascript file

--- bam_ad_system/public/js/app.js
+++ bam_ad_system/public/js/app.js
@@ -1,5 +1,6 @@
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_countdown
 // Update the count down every 1 second
+//First get the data from the span using the id, then convert everthing to integer, next milliseconds, add all the data with the current day
 var days_remaining = document.getElementById('days_remaining').innerText;
 var hours_remaining = document.getElementById('hours_remaining').innerText;
 var minutes_remaining = document.getElementById('minutes_remaining').innerText;
@@ -18,26 +19,21 @@ var countDownDate = today + total_time_get;
 
 var x = setInterval(function() {
 
-var now = new Date().getTime();
+	var now = new Date().getTime();
 
-// Find the distance between now and the count down date
-var remaining =  countDownDate - now;
+	// Find the difference between now and the count down date
+	var remaining =  countDownDate - now;
 
-// Time calculations for days, hours, minutes and seconds
-var days = Math.floor(remaining / (1000 * 60 * 60 * 24));
-var hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(remaining / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((remaining % (1000 * 60)) / 1000);
 
-// Output the result in an element with id="demo"
-document.getElementById("days_remaining").innerHTML = days;
-document.getElementById("hours_remaining").innerHTML = hours;
-document.getElementById("minutes_remaining").innerHTML = minutes;
-document.getElementById("seconds_remaining").innerHTML = seconds;
+	// Output the result in an clock section
+	document.getElementById("days_remaining").innerHTML = days;
+	document.getElementById("hours_remaining").innerHTML = hours;
+	document.getElementById("minutes_remaining").innerHTML = minutes;
+	document.getElementById("seconds_remaining").innerHTML = seconds;
 
-// 	// If the count down is over, write some text 
-	// if (remaining < 0) {
-	// clearInterval(x);
-	// document.getElementById("demo").innerHTML = "EXPIRED";
-	// }
 }, 1000);
